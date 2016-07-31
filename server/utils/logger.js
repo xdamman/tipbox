652423c49118150183f4f0601d775d7ff06689ab
mdp <m@mdp.im>
2016-07-31 13:04:03 -0400

    Add in winston logger

---
+++ server/utils/logger.js
@@ -0,0 +1,22 @@
+var winston = require('winston')
+
+var sLogger;
+
+function setup() {
+  var env = process.env.NODE_ENV || 'development'
+  var logLevel = require('../config/settings')(env)['logLevel'] || 'debug'
+  sLogger = new winston.Logger({
+    level: logLevel,
+    transports: [
+      new (winston.transports.Console)()
+    ]
+  });
+  return sLogger
+}
+
+exports.get = function() {
+  if (typeof sLogger !== 'undefined') {
+    return sLogger
+  }
+  return setup()
+}
