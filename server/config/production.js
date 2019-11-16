5bf3e5f88944068fe81beddab8da31c9fbaaa312
Mark Percival <m@mdp.im>
2019-11-16 13:58:28 +0000

    More simplification

--- server/config/production.js
+++ server/config/production.js
@@ -4,8 +4,5 @@ var port = process.env.PORT || 3000
 var logger = require('../utils/logger').instance()
 
 module.exports = function(app) {
-  app.use(function(req, res, next) {
-    logger.info('%s %s %s', req.method, req.url, req.path);
-    next();
-  });
+  app.set('staticMaxAge', 0);
 };
--- server/config/routes.js
+++ server/config/routes.js
@@ -6,11 +6,10 @@ module.exports = function(app) {
   app.use(function(req, res, next) {
 
     // Default to the request protocol and hostName as the origin
-    var protocol = req.protocol // Allow for nonSSL for cases like tor
     var host = app.config.settings.domain || req.get('host')
-    var origin = protocol + '://' + host
+    var origin = 'https://' + host
 
-    // Let us override this in certain cases
+    // Let us override this in certain cases (eg. HTTP over Tor)
     if (app.config.settings.origin) {
       origin = app.config.settings.origin
     }
@@ -22,7 +21,7 @@ module.exports = function(app) {
   });
 
   var src = (app.set('env') == 'development') ? 'src' : 'dist';
-  app.use('/', express.static(__dirname+"/../../frontend/"+src+"/", {maxAge: app.get('staticMaxAge')}));
+  app.use('/', express.static(__dirname+"/../../frontend/"+src+"/", {maxAge: 0}));
 
   app.use(middlewares.pgp);
 
